59cac51f7dcb457c3fc34d3282e9256a9dd5310a
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-04-27 22:18:55 -0300

    teste

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -1,11 +1,11 @@
 import { http } from './config';
 
-export default {
-    list: function () {
+export default UsuarioService = {
+    list: function() {
         return http.get("usuario/list");
     },
 
-    login: function (dadosUsuario) {
+    login: function(dadosUsuario) {
         // {
         //  "usuario": "sc@email.com",
         //  "senha": "123@123"
---
+++ src/views/UsuarioForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="form-group">
+      <label for="">Nome</label>
+      <input
+        type="text"
+        name="nome"
+        v-model="usuario.nome"
+        class="form-control"
+        placeholder="Nome"
+        aria-describedby="helpId"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+  </section>
+</template>
+
+<script>
+import { Usuario } from "@/models/Usuario";
+import UsuarioService from '@/services/usuario';
+var user = new Usuario();
+export default {
+
+    data(){
+        return{
+            usuario = user
+        }
+    }
+
+};
+</script>
+
+<style>
+</style>
