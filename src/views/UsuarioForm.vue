d39c284564d91e933c859eadcff25c8998fd4ffb
fMoreira <fabianomoreira.net@gmail.com>
2022-04-28 01:06:45 -0300

    2022-04-27

--- src/views/UsuarioForm.vue
+++ src/views/UsuarioForm.vue
@@ -8,10 +8,100 @@
         name="nome"
         v-model="usuario.nome"
         class="form-control"
-        placeholder="Nome"
+        placeholder=""
         aria-describedby="helpId"
       />
       <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="form-group">
+      <label for="">E-mail</label>
+      <input
+        type="email"
+        name="email"
+        v-model="usuario.email"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="form-group">
+      <label for="">Senha</label>
+      <input
+        type="password"
+        name="senha"
+        v-model="usuario.senha"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="form-group">
+      <label for="">Data Nasc.</label>
+      <input
+        type="date"
+        name="data_nasc"
+        v-model="usuario.data_nasc"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="form-group">
+      <label for="">Foto Perfil</label>
+      <input
+        type="file"
+        name="foto"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+        multiple
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+    <button @click="sendFile">Send</button>
+
+    <div class="form-group">
+      <label for="">Tel</label>
+      <input
+        type="number"
+        name="tel"
+        v-model="usuario.tel"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="form-group">
+      <label for="">CPF</label>
+      <input
+        type="number"
+        name="cpf"
+        v-model="usuario.cpf"
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+        max="99999999999"
+        min="00000000000"
+      />
+      <small id="helpId" class="text-muted"></small>
+    </div>
+
+    <div class="mb-3">
+      <button type="button" class="btn btn-primary mx-1" @click="cadastrar()">
+        Cadastrar
+      </button>
+      <button type="button" class="btn btn-danger" @click="$router.push('/')">
+        Sair
+      </button>
     </div>
   </section>
 </template>
@@ -33,7 +123,27 @@ export default {
   },
   methods: {
     cadastrar() {
-      UsuarioService.add(this.usuario);
+      UsuarioService.add(this.usuario)
+        .then((res) => {
+          console.log(res);
+          alert("Cadastrado!", res);
+        })
+        .catch((err) => {
+          console.error(err);
+          alert("Erro ao cadastrar!");
+        });
+    },
+    async sendFile() {
+      let dataForm = new FormData();
+      for (let file of this.$refs.files.files) {
+        dataForm.append(`file`, file);
+      }
+      let res = await fetch(`http://localhost/api/upload`, {
+        method: "POST",
+        body: dataForm,
+      });
+      let data = await res.json();
+      console.log(data);
     },
   },
 };
